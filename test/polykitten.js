dc3c7254b3228cb6e8f7654262e3dfb7c347acd7
hiromi-nagamoto <github@cats-in.space>
2024-01-04 01:46:49 +0900

    --network=%npm_config_network%

--- test/polykitten.js
+++ test/polykitten.js
@@ -1,15 +1,15 @@
 const PolyKitten = artifacts.require("./PolyKitten.sol");
-
+// openzeppelinライブラリには.set().get.call()メソッドが存在しないため。
 contract("PolyKitten", accounts => {
   it("...should store the value 89.", async () => {
-    const polyKittenInstance = await PolyKitten.deployed();
+    // const polyKittenInstance = await PolyKitten.deployed();
 
     // Set value of 89
-    await polyKittenInstance.set(89, { from: accounts[0] });
+    // await polyKittenInstance.set(89, { from: accounts[0] });
 
     // Get stored value
-    const storedData = await polyKittenInstance.get.call();
+    // const storedData = await polyKittenInstance.get.call();
 
-    assert.equal(storedData, 89, "The value 89 was not stored.");
+    // assert.equal(storedData, 89, "The value 89 was not stored.");
   });
 });
